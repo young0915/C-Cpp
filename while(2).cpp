37e511a07c612e2d278e69d245eeacdcfb4c723b
young0915 <38437784+young0915@users.noreply.github.com>
2019-11-05 21:46:22 +0900

    Add files via upload

---
+++ while(2).cpp
@@ -0,0 +1,166 @@
+#include <stdio.h>
+
+//int main(void)
+//{
+//	int num;
+//	printf("입력 : ");
+//	scanf_s("%d", &num);
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		for (int j = 0; j < i + 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//입력: 4
+//*
+//**
+//***
+//****
+//
+//C : \Users\goddn\source\repos\studyonee\Debug\studyonee.exe(15124 프로세스)이(가) 0 코드로 인해 종료되었습니다.
+//이 창을 닫으려면 아무 키나 누르세요.
+//
+//}
+
+//int main(void)
+//{
+//	int i = 0, result = 0;
+//	int num=0;
+//	
+//	
+//
+//	while (i<5)
+//	{
+//		while (num <= 0)
+//		{
+//			printf("0보다 큰 수를 입력(%d 번째)", i + 1);
+//			scanf_s("%d", &num);
+//		}
+//		result += num;
+//		num = 0;
+//		i++;
+//	}
+//	
+//	printf("결과 %d", result);
+//	
+//
+//	return 0;
+//}
+//int main(void)
+//{
+//	int num;
+//	int result=0;
+//	int i = 0;
+//	int s;
+//
+//	printf("몇 과목 : ");
+//	scanf_s("%d", &num);
+//
+//	while (i++ < num)
+//	{
+//		printf(" 점수를 입력하시오: \n");
+//		scanf_s("%d", &s);
+//		result += s;
+//	}
+//	printf("결과 값 : %f", (double)result / num);
+//	return 0;
+//}
+//int main(void)
+//{
+//	int dan;
+//	int i = 10;
+//	printf("몇 단?");
+//	scanf_s("%d", &dan);
+//	printf("\n");
+//
+//	while (0<i)
+//	{
+//		printf("%d * %d = %d\n", i, dan, i * dan);
+//		i--;
+//	}
+//	return 0;
+//
+//	몇 단 ? 5
+//
+//		10 * 5 = 50
+//		9 * 5 = 45
+//		8 * 5 = 40
+//		7 * 5 = 35
+//		6 * 5 = 30
+//		5 * 5 = 25
+//		4 * 5 = 20
+//		3 * 5 = 15
+//		2 * 5 = 10
+//		1 * 5 = 5
+//
+//		C:\Users\goddn\source\repos\studyonee\Debug\studyonee.exe(17956 프로세스)이(가) 0 코드로 인해 종료되었습니다.
+//		이 창을 닫으려면 아무 키나 누르세요.
+//
+//}
+/*
+int main(void)
+{
+	int i = 0, result = 0;
+	int num;
+	printf("정수를 입력하세요");
+	scanf_s("%d", &num);
+	printf("\n");
+	result += num;
+
+	while (num != 0)
+	{
+		printf("정수를 입력하세요");
+		scanf_s("%d", &num);
+		printf("\n");
+		result += num;
+	}
+	printf("결과 %d", result);
+
+	/*
+	정수를 입력하세요5
+
+정수를 입력하세요4
+
+정수를 입력하세요8
+
+정수를 입력하세요0
+
+결과 17
+C:\Users\goddn\source\repos\studyonee\Debug\studyonee.exe(23644 프로세스)이(가) 0 코드로 인해 종료되었습니다.
+이 창을 닫으려면 아무 키나 누르세요.
+
+	*/
+//}*/
+
+//
+//int main(void)
+//{
+//	int num;
+//	int i = 1;
+//	printf("정수를 입력하시오");
+//	scanf_s("%d", &num);
+//
+//	while (i <=num)
+//	{
+//		printf("%d * 3 = %d \n", i, i*3);
+//		i++;
+//	}
+//	return 0;
+//
+//	/*
+//	정수를 입력하시오 5
+//1 * 3 = 3
+//2 * 3 = 6
+//3 * 3 = 9
+//4 * 3 = 12
+//5 * 3 = 15
+//
+//C:\Users\goddn\source\repos\studyonee\Debug\studyonee.exe(19088 프로세스)이(가) 0 코드로 인해 종료되었습니다.
+//이 창을 닫으려면 아무 키나 누르세요.
+//*/
+//}
+
