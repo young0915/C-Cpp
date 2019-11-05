ea28ebbc05ce6d23e9b994f29cecf9ded5b7b096
young0915 <38437784+young0915@users.noreply.github.com>
2019-11-06 02:39:13 +0900

    Add files via upload

---
+++ 제어문(if문).cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+//int main(void)
+//{
+//	int num1, num2;
+//	int result = 0;
+//
+//	printf("입력");
+//	scanf_s("%d %d", &num1, &num2);
+//	printf("\n");
+//
+//	result = num1 > num2 ? num1 - num2 : num2 - num1;
+//	printf("%d", result);
+//	return 0;
+//}
+//int main(void)
+//{
+//	int score;
+//	printf("점수를 입력하시오");
+//	scanf_s("%d", &score);
+//
+//	if (score >= 90)
+//		printf("A");
+//	else if (score >= 80)
+//		printf("B");
+//	else if (score >= 70)
+//		printf("C");
+//	else
+//		printf("D");
+//	return 0;
+//}
+/*int main(void)
+{
+	int num1, num2;
+	int result = 0;
+
+	printf("정수를 입력하시오");
+	scanf_s("%d %d", &num1, &num2);
+
+	if (num1 > num2)
+		printf("%d", num1 - num2);
+	else
+		printf("%d", num2 - num1);
+	return 0;
+}*/
+//int main(void)
+//{
+//	int num;
+//
+//	for (num = 1; num <= 100; num++)
+//	{
+//		if(num % 7 == 0 || num%9 == 0)
+//		printf("7의 배수 또는 9의 배수 %d\n", num);
+//		
+//	}
+//
+//	return 0;
+//}
